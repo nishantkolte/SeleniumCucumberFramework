7160b05165916bc73bfa7a3ca46211e00b8dc92c
NISH <NISH@LAPTOP-G0ITPRBD>
2019-05-09 00:44:50 +0530

    COMMIT COMMENT

--- src/main/java/inputdata/Testdata.java
+++ src/main/java/inputdata/Testdata.java
@@ -9,7 +9,7 @@ public Testdata(String env){
 	if (env.equalsIgnoreCase("QAI")){
 	
 		//-----QA TEST DATA-----// Set QA test data here
-		 url="http://newtours.demoaut.com/mercurysignon.php";
+		 url="http://newtours.demoaut.com/mercurysignon.php";//
 		 username="batman";
 		 password="batman";			
 	}
